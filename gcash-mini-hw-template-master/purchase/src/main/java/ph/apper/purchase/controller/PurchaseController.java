2f62b793f8ab361e666c96425bb0b0d8d3d45b20
gpagatpatan <gerome.pagatpatan@mynt.xyz>
2021-04-08 22:13:21 +0800

    Update purchase controller

--- gcash-mini-hw-template-master/purchase/src/main/java/ph/apper/purchase/controller/PurchaseController.java
+++ gcash-mini-hw-template-master/purchase/src/main/java/ph/apper/purchase/controller/PurchaseController.java
@@ -14,6 +14,7 @@ import ph.apper.product.service.ProductService;
 import javax.validation.Valid;
 import java.util.List;
 
+//Purchase
 @RestController
 @RequestMapping("purchase")
 public class PurchaseController {
