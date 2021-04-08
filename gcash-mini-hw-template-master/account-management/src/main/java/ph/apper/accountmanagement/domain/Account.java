7ee2df3e262361633136674f286244f4745a9931
jratienza <johnashly.atienza@mynt.xyz>
2021-04-08 20:44:13 +0800

    add Account domain

--- gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/domain/Account.java
+++ gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/domain/Account.java
@@ -1,4 +1,34 @@
 package ph.apper.accountmanagement.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
 public class Account {
+    @Id
+    private String id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private BigDecimal balance;
+    private boolean isVerified;
+
+    private LocalDateTime dateRegistered;
+    private LocalDateTime dateVerified;
+    private LocalDateTime lastLogin;
+
+    public Account(String id){
+        this.id = id;
+    }
+
+    public Account(){
+
+    }
+
 }
