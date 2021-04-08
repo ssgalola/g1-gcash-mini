3858a66c78cded2f8a595c59836a025ba64895cf
Mark Rhoy <makrhoy.sabal@mynt.xyz>
2021-04-08 18:57:55 +0800

    implemented verification and authentication endpoint

--- gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/domain/Account.java
+++ gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/domain/Account.java
@@ -1,4 +1,15 @@
 package ph.apper.accountmanagement.domain;
 
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
 public class Account {
+    private UUID accountId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private String verificationCode;
 }
