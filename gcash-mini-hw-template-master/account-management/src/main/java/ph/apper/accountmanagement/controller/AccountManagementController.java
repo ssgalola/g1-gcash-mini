e1172fc9d09f4ac2e843b7e0eb6fcb91c9a976da
jratienza <johnashly.atienza@mynt.xyz>
2021-04-09 05:11:32 +0800

    add create account and get account request mapping

--- gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/controller/AccountManagementController.java
+++ gcash-mini-hw-template-master/account-management/src/main/java/ph/apper/accountmanagement/controller/AccountManagementController.java
@@ -1,14 +1,16 @@
 package ph.apper.accountmanagement.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ph.apper.accountmanagement.domain.Account;
+import ph.apper.accountmanagement.payload.AccountRequest;
 import ph.apper.accountmanagement.payload.AuthenticateAccount;
+import ph.apper.accountmanagement.payload.NewAccountResponse;
 import ph.apper.accountmanagement.payload.VerifyAccount;
+import ph.apper.accountmanagement.service.AccountService;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -18,8 +20,26 @@ import java.util.UUID;
 @RestController
 @RequestMapping("account")
 public class AccountManagementController {
+    private static  final Logger LOGGER = LoggerFactory.getLogger(AccountManagementController.class);
+    private final AccountService accountService;
 
+    public AccountManagementController(AccountService accountService){
+        this.accountService = accountService;
+    }
     private List<Account> accountList = new ArrayList<>();
+
+    @PostMapping
+    public ResponseEntity<Object> createAccount(@RequestBody AccountRequest request){
+        LOGGER.info("Create account request received.");
+        NewAccountResponse response = accountService.addAccount(request);
+        LOGGER.info("Account Created");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("{accountId}")
+    public ResponseEntity<Object> getAccount(@PathVariable("accountId") String accountId){
+        return new ResponseEntity<>(accountService.getAccountDetails(accountId), HttpStatus.OK);
+    }
 
     @PostMapping("/verify")
     public ResponseEntity<Object> verifyAccount(@RequestBody VerifyAccount request){
